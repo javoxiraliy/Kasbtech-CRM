98d873809d2242039ce39dae7014832aabf5bc6c
Javoxir0804 <sferrell244@gmail.com>
2026-08-06 17:33:35 +0500

    clean: remove scratch test file

--- server/scratch_test.js
+++
@@ -1,5 +0,0 @@
-const pdfParse = require('pdf-parse');
-const mammoth = require('mammoth');
-const XLSX = require('xlsx');
-
-console.log('PDF Parse, Mammoth, XLSX loaded successfully inside server!');
